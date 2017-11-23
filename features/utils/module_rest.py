39390093d51f02d3364b302a4486fdb44633006b
patzygit <33137804+patzygit@users.noreply.github.com>
2017-11-23 10:56:29 -0400

    Changes in item feature,  post
    Changes in item feature,  post is changed to use the post an rename 'payload' to 'json'

--- features/utils/module_rest.py
+++ features/utils/module_rest.py
@@ -1,16 +1,26 @@
 import requests
 import json
 
-def perform_get_request(host, hearders=None):
-     r = requests.get(host, hearder=hearders)
-     return  r.json()
+def perform_get_request(host, headers=None):
+    r = requests.get(host, headers=headers)
+    return r.json()
 
+def perform_request(method, endpoint, headers='', body=''):
+#    print("endpoint", endpoint)
+#    print ("header", headers)
+#    print("body", body)
+    if method == 'GET':
+	    result = requests.get(endpoint, headers=headers)
+    if method == 'POST':
+	    result = requests.post(endpoint, headers=headers,json=body)
+       # print ("resutl", result)
+    if method == 'PUT':
+        result = requests.put(endpoint, payload=body, headers=headers)
+    if method == 'DELETE':
+        result = requests.delete(endpoint, headers=headers)
+    return result
 
-def perform_request(method, endpoint):
-    if  method == 'GET':
-        requests.get(endpoint)
-    if  method == 'POST':
-        requests.post()
-
-
-
+def generateFileJson(path, fileName, data):
+    filePathNameWExt = path + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
