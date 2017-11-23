cac7b1c11ff64c48e1ffbd6cc5bcb2cc7760ed81
patzygit <33137804+patzygit@users.noreply.github.com>
2017-11-23 10:54:21 -0400

    Add files via upload
    Changes in item feature, steps get and post

--- features/steps/item_step_addItem.py
+++ features/steps/item_step_addItem.py
@@ -1,20 +1,24 @@
 from compare import *
 from utils.module_rest import *
-#from jsondiff import diff
+from jsondiff import diff
 from behave import given, when, then
+import jsondiff
+import json
 
-@when(u'I send POST items request to add a new item')
+
+@given(u'I have a pyloadBody')
+def step_impl(context):
+    context.pyloadBody = json.loads(context.text)
+    print("*** ", context.text)
+
+@when(u'I send {postmethod} items request to add a new item')
 def step_impl(context,postmethod):
     context.method   = postmethod
     context.url      = context.host + context.rootPath + context.servicemethod
     context.headers  = context.token
-    context.response = perform_request(context.method, context.url, context.headers, context.pyloadRequest)
-    generateFileJson("data/", "dataAddItems", context.response.json())
-
-@when(u'I have a pyloadBody')
-def step_impl(context):
-    #context.pyloadRequest = json.loads(context.text)
-    print("*** ",context.text)
+    context.pyloadrequest = context.pyloadBody
+    context.response = perform_request(context.method, context.url, context.headers, context.pyloadrequest)
+    generateFileJson("data/", "dataAddItem", context.response.json())
 
 @then(u'I got response body of new item')
 def step_impl(context):
--- features/steps/item_step_getItem.py
+++ features/steps/item_step_getItem.py
@@ -1,4 +1,7 @@
 from compare import *
+from jsondiff import diff
+from utils.json_order import *
+
 from utils.module_rest import *
 from behave import given, when, then
 
@@ -28,6 +31,22 @@ def step_impl(context, method):
     context.response = perform_request(context.method, context.url, context.headers)
     generateFileJson("data/", "dataIdItems", context.response.json())
 
+@then(u'response body should contain item information of specified item as')
+def step_impl(context):
+    context.response_get = json.loads(context.text)
+    #print ("***//data", context.response_get)
+    #print ("repons -re " , context.response.json())
+    boolean = ordered(context.response_get) != ordered(context.response.json())
+    #print ("bool",boolean)
+    assert boolean == True
+ #   expect(int(context.response_get)).to_equal(context.response.json())
+
+@then(u'response body should empty of specified item as')
+def step_impl(context):
+ a=diff(context.text,context.response)
+ print ("text", a)
+ expect(a).to_equal({})
+
 #/items/10028551/RootItem.json
 @given(u'I have a service with root id "{servicemethod}"')
 def step_impl(context, servicemethod):
@@ -39,18 +58,29 @@ def step_impl(context, method):
     context.url      = context.host + context.rootPath + context.servicemethod
     context.headers  = context.token
     context.response = perform_request(context.method, context.url, context.headers)
+    print ("rootId",context.response)
     generateFileJson("data/", "dataRootIdItems", context.response.json())
+
+@then(u'response body should contain item information of unchecked item as')
+def step_impl(context):
+    context.response_get = json.loads(context.text)
+    #print ("***//data", context.response_get)
+    #print ("repons -re " , context.response.json())
+    boolean = ordered(context.response_get) != ordered(context.response.json())
+    #print ("bool",boolean)
+    assert boolean == True
 
 #/items/10048278/DoneRootItem.json
 @given(u'I have a service with item id "{servicemethod}"')
 def step_impl(context, servicemethod):
     context.servicemethod = servicemethod
 #failed
-@when(u'I send {method} item request to get root parent for done item')
+@when(u'I send {method} item request to get an error for done item that does not exist')
 def step_impl(context, method):
     context.method   = method
     context.url      = context.host + context.rootPath + context.servicemethod
     context.headers  = context.token
     context.response = perform_request(context.method, context.url, context.headers)
-    print ("////", context.response)
-    generateFileJson("data/", "dataDRootIdItems", context.response.json())
+    #print ("////", context.response_dr)
+    print("////status", context.response.status_code)
+   # generateFileJson("data/", "dataDoneRootIdItems", context.response_dr.json())
